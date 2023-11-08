34e22dc8934a96a20a554d53efe7b78362c0fcc3
JadirMiramontes <jadirescuela23@gmail.com>
2023-11-08 12:14:03 -0600

    Agregar archivos de proyecto.

---
+++ Compilador/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Compilador
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rtCodigo = new System.Windows.Forms.RichTextBox();
+			this.dgvTablaSimbolos = new System.Windows.Forms.DataGridView();
+			this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Palabra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dgvErrores = new System.Windows.Forms.DataGridView();
+			this.LineaCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ErrorCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Linea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.btnAnalisisLexico = new System.Windows.Forms.Button();
+			this.btnAnalizadorSintactico = new System.Windows.Forms.Button();
+			this.rtCodigoIntermedio = new System.Windows.Forms.RichTextBox();
+			this.btnCodigoIntermedio = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvTablaSimbolos)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvErrores)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// rtCodigo
+			// 
+			this.rtCodigo.Location = new System.Drawing.Point(13, 13);
+			this.rtCodigo.Name = "rtCodigo";
+			this.rtCodigo.Size = new System.Drawing.Size(394, 375);
+			this.rtCodigo.TabIndex = 0;
+			this.rtCodigo.Text = "";
+			// 
+			// dgvTablaSimbolos
+			// 
+			this.dgvTablaSimbolos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvTablaSimbolos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tipo,
+            this.Palabra});
+			this.dgvTablaSimbolos.Location = new System.Drawing.Point(413, 13);
+			this.dgvTablaSimbolos.Name = "dgvTablaSimbolos";
+			this.dgvTablaSimbolos.Size = new System.Drawing.Size(375, 100);
+			this.dgvTablaSimbolos.TabIndex = 1;
+			// 
+			// Tipo
+			// 
+			this.Tipo.HeaderText = "Tipo";
+			this.Tipo.Name = "Tipo";
+			// 
+			// Palabra
+			// 
+			this.Palabra.HeaderText = "Palabra";
+			this.Palabra.Name = "Palabra";
+			// 
+			// dgvErrores
+			// 
+			this.dgvErrores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.LineaCodigo,
+            this.ErrorCodigo});
+			this.dgvErrores.Location = new System.Drawing.Point(413, 134);
+			this.dgvErrores.Name = "dgvErrores";
+			this.dgvErrores.Size = new System.Drawing.Size(375, 107);
+			this.dgvErrores.TabIndex = 5;
+			// 
+			// LineaCodigo
+			// 
+			this.LineaCodigo.HeaderText = "Linea";
+			this.LineaCodigo.Name = "LineaCodigo";
+			// 
+			// ErrorCodigo
+			// 
+			this.ErrorCodigo.HeaderText = "Error";
+			this.ErrorCodigo.Name = "ErrorCodigo";
+			this.ErrorCodigo.Width = 340;
+			// 
+			// Linea
+			// 
+			this.Linea.HeaderText = "Linea";
+			this.Linea.Name = "Linea";
+			// 
+			// btnAnalisisLexico
+			// 
+			this.btnAnalisisLexico.Location = new System.Drawing.Point(13, 394);
+			this.btnAnalisisLexico.Name = "btnAnalisisLexico";
+			this.btnAnalisisLexico.Size = new System.Drawing.Size(75, 44);
+			this.btnAnalisisLexico.TabIndex = 3;
+			this.btnAnalisisLexico.Text = "Analisis Lexico";
+			this.btnAnalisisLexico.UseVisualStyleBackColor = true;
+			this.btnAnalisisLexico.Click += new System.EventHandler(this.btnAnalisisLexico_Click);
+			// 
+			// btnAnalizadorSintactico
+			// 
+			this.btnAnalizadorSintactico.Location = new System.Drawing.Point(95, 394);
+			this.btnAnalizadorSintactico.Name = "btnAnalizadorSintactico";
+			this.btnAnalizadorSintactico.Size = new System.Drawing.Size(75, 44);
+			this.btnAnalizadorSintactico.TabIndex = 4;
+			this.btnAnalizadorSintactico.Text = "Analisis sintactico";
+			this.btnAnalizadorSintactico.UseVisualStyleBackColor = true;
+			this.btnAnalizadorSintactico.Click += new System.EventHandler(this.btnAnalizadorSintactico_Click);
+			// 
+			// rtCodigoIntermedio
+			// 
+			this.rtCodigoIntermedio.BackColor = System.Drawing.Color.Black;
+			this.rtCodigoIntermedio.ForeColor = System.Drawing.Color.White;
+			this.rtCodigoIntermedio.Location = new System.Drawing.Point(414, 270);
+			this.rtCodigoIntermedio.Name = "rtCodigoIntermedio";
+			this.rtCodigoIntermedio.Size = new System.Drawing.Size(374, 118);
+			this.rtCodigoIntermedio.TabIndex = 6;
+			this.rtCodigoIntermedio.Text = "";
+			// 
+			// btnCodigoIntermedio
+			// 
+			this.btnCodigoIntermedio.Location = new System.Drawing.Point(188, 395);
+			this.btnCodigoIntermedio.Name = "btnCodigoIntermedio";
+			this.btnCodigoIntermedio.Size = new System.Drawing.Size(75, 44);
+			this.btnCodigoIntermedio.TabIndex = 7;
+			this.btnCodigoIntermedio.Text = "Codigo Intermedio";
+			this.btnCodigoIntermedio.UseVisualStyleBackColor = true;
+			this.btnCodigoIntermedio.Click += new System.EventHandler(this.btnCodigoIntermedio_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnCodigoIntermedio);
+			this.Controls.Add(this.rtCodigoIntermedio);
+			this.Controls.Add(this.btnAnalizadorSintactico);
+			this.Controls.Add(this.btnAnalisisLexico);
+			this.Controls.Add(this.dgvErrores);
+			this.Controls.Add(this.dgvTablaSimbolos);
+			this.Controls.Add(this.rtCodigo);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dgvTablaSimbolos)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvErrores)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox rtCodigo;
+		private System.Windows.Forms.DataGridView dgvTablaSimbolos;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Palabra;
+		private System.Windows.Forms.DataGridView dgvErrores;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Linea;
+		private System.Windows.Forms.Button btnAnalisisLexico;
+		private System.Windows.Forms.Button btnAnalizadorSintactico;
+		private System.Windows.Forms.DataGridViewTextBoxColumn LineaCodigo;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ErrorCodigo;
+		private System.Windows.Forms.RichTextBox rtCodigoIntermedio;
+		private System.Windows.Forms.Button btnCodigoIntermedio;
+	}
+}
+
---
+++ Compilador/Form1.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compilador
+{
+	public partial class Form1 : Form
+	{
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void btnAnalisisLexico_Click(object sender, EventArgs e)
+		{
+			string sourceCode = rtCodigo.Text;
+			List<Token> symbolTable = AnalizarL(sourceCode);
+
+			dgvTablaSimbolos.Rows.Clear();
+
+			foreach (Token token in symbolTable)
+			{
+				dgvTablaSimbolos.Rows.Add(token.Type, token.Value);
+			}
+		}
+
+
+		/*Analisis Lexico*/
+		private List<Token> AnalizarL(string sourceCode)
+		{
+			List<Token> symbolTable = new List<Token>();
+
+			string keywordPattern = "\\b(si|sino|mientras|para|entero|cadena|bool|publico|privado|clase|flotante)\\b";
+			string operatorPattern = @"[+\-\*\/\=]";
+			string numberPattern = "\\b[0-9]+\\b";
+			string whitespacePattern = "\\s+";
+
+			string pattern = $"({keywordPattern}|{operatorPattern}|{numberPattern}|{whitespacePattern})";
+
+			MatchCollection matches = Regex.Matches(sourceCode, pattern);
+
+			foreach (Match match in matches)
+			{
+				if (!string.IsNullOrWhiteSpace(match.Value))
+				{
+					TokenType type = GetTokenType(match.Value);
+					symbolTable.Add(new Token(type, match.Value));
+				}
+			}
+			return symbolTable;
+		}
+
+
+		private TokenType GetTokenType(string tokenValue)
+		{
+			if (Regex.IsMatch(tokenValue, "\\b(si|sino|mientras|para|publico|privado|clase)\\b"))
+			{
+				return TokenType.Palabra_Clave;
+			}
+			else if (Regex.IsMatch(tokenValue, "\\b(entero|cadena|bool|flotante)\\b"))
+			{
+				return TokenType.Tipo;
+			}
+			else if (Regex.IsMatch(tokenValue, "\\b[0-9]+\\b"))
+			{
+				return TokenType.Numero;
+			}
+			else if (Regex.IsMatch(tokenValue, "\\s+"))
+			{
+				return TokenType.Espacio_Blanco;
+			}
+			else if (Regex.IsMatch(tokenValue, @"[+\-\*\/\=]"))
+			{
+				return TokenType.Operador;
+			}
+			else
+			{
+				return TokenType.Otros;
+			}
+		}
+
+		enum TokenType
+		{
+			Palabra_Clave, Tipo, Numero, Espacio_Blanco, Operador, Otros, Error
+		}
+
+		class Token
+		{
+			public TokenType Type { get; }
+			public string Value { get; }
+
+			public Token(TokenType type, string value)
+			{
+				Type = type;
+				Value = value;
+			}
+		}
+
+		private void btnAnalizadorSintactico_Click(object sender, EventArgs e)
+		{
+
+			string sourceCode = rtCodigo.Text;
+			string[] lines = sourceCode.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+			List<Error> errorList = AnalizarSintaxis(lines);
+
+			dgvErrores.Rows.Clear();
+
+			foreach (Error error in errorList)
+			{
+				dgvErrores.Rows.Add(error.LineNumber, error.Message);
+			}
+		}
+
+		private List<Error> AnalizarSintaxis(string[] lines)
+		{
+			List<Error> errorList = new List<Error>();
+			HashSet<string> keywords = new HashSet<string> { "si", "sino", "mientras", "para" };
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				string line = lines[i].Trim(); // Eliminar espacios en blanco al principio y al final de la línea
+				if (!string.IsNullOrWhiteSpace(line))
+				{
+					if (!line.EndsWith(")") && keywords.Any(keyword => line.EndsWith(keyword)))
+					{
+						errorList.Add(new Error(i + 1, $"Error: Palabra clave '{line.Split(' ')[0]}' mal declarada. Falta paréntesis de cierre."));
+					}
+
+					string[] words = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+					bool declarationFound = false;
+
+					foreach (string word in words)
+					{
+						if (declarationFound && word.EndsWith("="))
+						{
+							errorList.Add(new Error(i + 1, $"Error: Variable '{word.Trim('=')}' no está completamente declarada en la línea {i + 1}."));
+						}
+
+						if (word.Contains(";"))
+						{
+							errorList.Add(new Error(i + 1, $"Error: No se deben usar ';' al finalizar las líneas."));
+						}
+
+						if (keywords.Contains(word))
+						{
+							declarationFound = true;
+							if (!word.EndsWith("("))
+							{
+								errorList.Add(new Error(i + 1, $"Error: Palabra clave '{word}' mal declarada. Falta paréntesis de apertura."));
+							}
+						}
+
+						if (keywords.Contains(word))
+						{
+							declarationFound = true;
+						}
+					}
+				}
+			}
+
+			return errorList;
+		}
+
+		class Error
+		{
+			public int LineNumber { get; }
+			public string Message { get; }
+			public Error(int lineNumber, string message)
+			{
+				LineNumber = lineNumber;
+				Message = message;
+			}
+		}
+
+		private void btnCodigoIntermedio_Click(object sender, EventArgs e)
+		{
+			string codigoFuente = rtCodigo.Text;
+			string keywordPattern = "\\b(si|sino|mientras|para|entero|cadena|bool|publico|privado|clase|flotante)\\b";
+			string operatorPattern = @"[+\-\*\/\=]";
+			string numberPattern = "\\b[0-9]+\\b";
+			string whitespacePattern = "\\s+";
+
+			Regex regexKeyword = new Regex(keywordPattern);
+			Regex regexOperator = new Regex(operatorPattern);
+			Regex regexNumber = new Regex(numberPattern);
+			Regex regexWhitespace = new Regex(whitespacePattern);
+
+			string[] tokens = regexWhitespace.Split(codigoFuente);
+			string codigoIntermedioPython = "";
+
+			foreach (string token in tokens)
+			{
+				if (!string.IsNullOrWhiteSpace(token))
+				{
+					if (regexKeyword.IsMatch(token))
+					{
+						// Convertir palabras clave de C# a Python
+						string palabraClavePython = ConvertirPalabraClave(token);
+						codigoIntermedioPython += palabraClavePython + " ";
+					}
+					else if (regexOperator.IsMatch(token))
+					{
+						// Mantener los operadores tal como están
+						codigoIntermedioPython += token + " ";
+					}
+					else if (regexNumber.IsMatch(token))
+					{
+						// Mantener los números tal como están
+						codigoIntermedioPython += token + " ";
+					}
+					else
+					{
+						// Mantener los identificadores tal como están
+						codigoIntermedioPython += token + " ";
+					}
+				}
+			}
+
+			// Mostrar el código intermedio en rtCodigoIntermedio
+			rtCodigoIntermedio.Text = codigoIntermedioPython;
+		}
+
+		private string ConvertirPalabraClave(string palabraClave)
+		{
+			// Realizar conversiones específicas de C# a Python para palabras clave
+			switch (palabraClave)
+			{
+				case "si":
+					return "if";
+				case "sino":
+					return "else";
+				case "mientras":
+					return "while";
+				case "para":
+					return "for";
+				case "entero":
+					return "int";
+				case "cadena":
+					return "str";
+				case "bool":
+					return "bool";
+				case "publico":
+					return "public";
+				case "privado":
+					return "private";
+				case "clase":
+					return "class";
+				case "flotante":
+					return "float";
+				default:
+					// Si no se encuentra una coincidencia, devolver la palabra clave tal como está
+					return palabraClave;
+			}
+		}
+
+	}
+}
